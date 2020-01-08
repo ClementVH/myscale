d198b4b0929e81875b80d9624fdbc4ebbc7f3d39
cvanehcke <clement.van-hecke@netatmo.com>
2020-01-08 14:12:53 +0100

    cvh: keep same filename, update size on new file drop

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,4 @@
-var fileUpload = document.getElementById("file_upload");
-fileUpload.addEventListener("change", function(){
-	var fileSvg = fileUpload.files[0];
-	var reader = new FileReader();
-	reader.readAsText(fileSvg);	
-	reader.onload = function(){
-		document.getElementById("svg_preview").innerHTML = reader.result;
-		document.getElementById("svg_form").style.display = "block";
-	}
-});
+var fileName;
 
 var playGround = function(){
 	var resizeValue = document.getElementById("resize_value").value;
@@ -15,9 +6,23 @@ var playGround = function(){
 	document.getElementsByTagName("svg")[0].setAttribute("width", resizeValue);
 	document.getElementsByTagName("svg")[0].setAttribute("height", resizeValue);
 	document.getElementById("download_wrapper").style.display = "block";
-	var fileRename = "icon-" + resizeValue + "px.png";
+	var fileRename = fileName + '.png';
 	document.getElementById("data").setAttribute("download", fileRename);
 }
+
+var fileUpload = document.getElementById("file_upload");
+fileUpload.addEventListener("change", function(){
+	fileName = fileUpload.value.split(/(\\|\/)/g).pop();
+	fileName = fileName.split('.')[0];
+	var fileSvg = fileUpload.files[0];
+	var reader = new FileReader();
+	reader.readAsText(fileSvg);
+	reader.onload = function(){
+		document.getElementById("svg_preview").innerHTML = reader.result;
+		document.getElementById("svg_form").style.display = "block";
+		playGround();
+	}
+});
 
 // SVG to DATA URL Library
 
